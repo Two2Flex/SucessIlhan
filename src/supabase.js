93114f64a7bb06357e9074f28d4e7000941915c1
23altintop <23altintop@23altintop.fr>
2025-03-31 15:57:51 +0200

    modif supa

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dcqqaceltrusyahakpng.supabase.co'; 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjcXFhY2VsdHJ1c3lhaGFrcG5nIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTE2MDU5MiwiZXhwIjoyMDQ0NzM2NTkyfQ.vm_1Qj7erC1WldEWUslAsiLaj1T2BzS8X6fgewezp10'; 
+const supabaseUrl = 'https://rqtwjlmdpvngkaqbovkk.supabase.co'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxdHdqbG1kcHZuZ2thcWJvdmtrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNDM1NDU3OSwiZXhwIjoyMDQ5OTMwNTc5fQ.91YHHyp4jfKR_Xdfh--vb87EAsFM2ZkkKKsCUEMPtz0'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
